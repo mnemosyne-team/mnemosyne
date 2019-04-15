6a7cd49bf0f049388a0d753077d44cf489bb82aa
Dmitriy Kharavets <kharavets123@mail.ru>
2019-04-15 22:20:13 +0300

    Added EMAIL_BACKEND to settings.py

--- mnemosyne/settings.py
+++ mnemosyne/settings.py
@@ -142,6 +142,7 @@ LOGIN_REDIRECT_URL = 'index'
 
 
 # SMTP settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
